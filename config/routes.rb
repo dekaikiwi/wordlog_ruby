c64a82f91265e2cd8a9b6eb3664287a5a7290cfa
Jonathan Glassey <Jono-Shuttlerock@Jonathans-MBP.fritz.box>
2015-10-01 23:09:01 +1300

    finished chapter 2, rspec for versioning header

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
   #API Definition
-  namespace :api, defaults: { format: :json }
+  namespace :api, defaults: { format: :json },
                             constraints: { subdomain: 'api' }, path: '/' do
-    # We are going to list our resources here
+    scope module: :v1,
+               constraints: ApiConstraints.new(version: 1, default: true) do
+
+      # We are going to list our resources here
+    end
+
   end
 end
