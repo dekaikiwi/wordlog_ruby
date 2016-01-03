7f3bfa154cd098a5ac57cb3558a15a1786927e89
Jonathan Glassey <jono@shuttlerock.com>
2016-01-04 00:34:03 +1300

    Adds show action the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
                constraints: ApiConstraints.new(version: 1, default: true) do
 
       # We are going to list our resources here
+      resources :users, :only => [:show]
     end
 
   end
